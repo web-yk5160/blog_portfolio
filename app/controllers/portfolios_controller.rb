9a2e6a7634976bc30fcfea55ae99c7b82380529e
web-yk5160 <rubystudy0123@gmail.com>
2019-05-25 23:42:59 +0900

    Integrated new and create actions for portfolios

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -2,4 +2,23 @@ class PortfoliosController < ApplicationController
   def index
     @portfolio_items = Portfolio.all
   end
+
+  def new
+    @portfolio_item = Portfolio.new
+  end
+
+  def create
+    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html { redirect_to portfolios_path, notice: 'ポートフォリオが作成されました。' }
+        # @blogはblog_path(@blog)と書き換えられる
+        # format.json { render :show, status: :created, location: @blog }
+      else
+        format.html { render :new }
+        # format.json { render json: @blog.errors, status: :unprocessable_entity }
+      end
+    end
+  end
 end
